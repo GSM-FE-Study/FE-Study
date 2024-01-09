1f367e9e200ecb58b0e44db28383cb50f6fcbf48
Lee-Seungje <moondgod@gmail.com>
2024-01-09 15:06:15 +0900

    안전지대

---
+++ Algorithm/seungje/안전지대/다른 사람의 풀이.js
@@ -0,0 +1,24 @@
+function solution(board) {
+  let outside = [
+    [-1, 0],
+    [-1, -1],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, 0],
+    [1, -1],
+    [1, 1],
+  ];
+  let safezone = 0;
+
+  board.forEach((row, y, self) =>
+    row.forEach((it, x) => {
+      if (it === 1) return false;
+      return outside.some(([oy, ox]) => !!self[oy + y]?.[ox + x])
+        ? false
+        : safezone++;
+    })
+  );
+
+  return safezone;
+}
