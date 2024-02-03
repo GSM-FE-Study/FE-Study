9881059971595e9a53fcd600afa73962f510f60f
TaerangLee <s22051@gsm.hs.kr>
2024-02-03 21:05:58 +0900

    홀짝에 따라 다른 값 반환하기

---
+++ Algorithm/taerang/홀짝에 따라 다른 값 반환하기/다른 사람의 풀이.js
@@ -0,0 +1,12 @@
+2;
+3;
+4;
+5;
+function solution(n) {
+  const array = Array(n)
+    .fill()
+    .map((x, idx) => idx + 1);
+  return n % 2 === 0
+    ? array.reduce((a, b) => (b % 2 === 0 ? a + Math.pow(b, 2) : a), 0)
+    : array.reduce((a, b) => (b % 2 === 0 ? a : a + b), 0);
+}
