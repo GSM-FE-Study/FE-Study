9c3d9f38d1912e4400e14a36ae24615735ffbe79
TaerangLee <s22051@gsm.hs.kr>
2024-02-10 15:53:16 +0900

    카운트 다운

--- Algorithm/taerang/카운트 다운/다른 사람의 풀이.js
+++ Algorithm/taerang/카운트 다운/다른 사람의 풀이.js
@@ -1,6 +1,1 @@
-const solution = (start, end) =>
-  Array(start - end + 1)
-    .fill(start)
-    .map((v, i) => v - i);
-
-//
+const solution = (start, end) => Array(start-end+1).fill(start).map((v,i)=>v-i);
