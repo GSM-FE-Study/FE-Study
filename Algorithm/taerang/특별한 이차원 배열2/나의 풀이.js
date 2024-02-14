fc3403eb8449d8eb5e6e46c6aa46c4941a5607c0
TaerangLee <s22051@gsm.hs.kr>
2024-02-14 15:30:28 +0900

    특별한 이차원 배열 2

---
+++ Algorithm/taerang/특별한 이차원 배열2/나의 풀이.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  let answer = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] !== arr[j][i]) {
+        answer = 0;
+      }
+    }
+  }
+
+  return answer;
+}
