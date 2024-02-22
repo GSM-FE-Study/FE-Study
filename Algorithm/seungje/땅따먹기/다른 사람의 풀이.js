68c95b7bb91ae1985f9b5f4fd62caca2d01fcbc2
Lee-Seungje <moondgod@gmail.com>
2024-02-22 12:21:39 +0900

    땅따먹기

---
+++ Algorithm/seungje/땅따먹기/다른 사람의 풀이.js
@@ -0,0 +1,17 @@
+function solution(land) {
+  var answer = 0;
+
+  return Math.max(
+    ...land.reduce(
+      (a, c) => {
+        return [
+          c[0] + Math.max(a[1], a[2], a[3]),
+          c[1] + Math.max(a[0], a[2], a[3]),
+          c[2] + Math.max(a[0], a[1], a[3]),
+          c[3] + Math.max(a[0], a[1], a[2]),
+        ];
+      },
+      [0, 0, 0, 0]
+    )
+  );
+}
