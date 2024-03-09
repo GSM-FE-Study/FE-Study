d9dcf833c9774d186e0e46b3cc69b1bcb968586f
gjaegyun <jaegyungim06@gmail.com>
2024-03-09 23:38:38 +0900

    소인수 분해

---
+++ Algorithm/jaegyun/소인수분해/나의 풀이.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = [];
+    
+    for (let i=2 ; i<=n ; i++){
+        while(n % i === 0){
+            n = n / i
+            if(n % i !== 0){
+                answer.push(i)
+            }
+            
+        }
+    }
+    return answer;
+}
